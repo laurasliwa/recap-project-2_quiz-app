10aa416b55df7d231bffe52a9085b7ace2c5a28d
Laura Sliwa <l.sliwa@gmx.de>
2024-05-03 11:46:05 +0200

    Add toggle for bookmark and answer button

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // phew… not a lot going on here. Please add some code!
+
+// toggle bookmark
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+// toggle answer
+
+const answerButton = document.querySelector('[data-js="answer-toggle"]');
+const answerView = document.querySelector('[data-js="answer-view"]');
+
+answerButton.addEventListener("click", () => {
+  answerView.classList.toggle("card__answer--active");
+});
